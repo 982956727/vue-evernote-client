e48f4202cf7b30d29d9a73757c1e35c65ac333f0
982956727 <982956727@qq.com>
2023-11-08 22:42:03 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <p>dialog 的文档</p>
+    <h1>示例1</h1>
+    <Dialog></Dialog>
   </div>
 </template>
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: { Dialog }
+}
+</script>
