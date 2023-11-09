778a981fea89c4f4275d2739f3ee5ae7c2510631
982956727 <982956727@qq.com>
2023-11-09 22:53:23 +0800

    indicator css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,6 +3,7 @@
     <div class="gulu-tabs-nav">
       <div class="gulu-tabs-nav-item" @click="select(t)"
         :class="{selected: t == selected}" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component class="gulu-tabs-content-item" 
@@ -52,6 +53,7 @@
       display: flex;
       color: $color;
       border-bottom: 1px solid $border-color;
+      position: relative;
       &-item {
         padding: 8px 0;
         margin: 0 16px;
@@ -62,6 +64,14 @@
         &.selected {
           color: $blue;
         }
+      }
+      &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
       }
     }
     &-content {
