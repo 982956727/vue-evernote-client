81fb3e9984d0df95b817d43e984f23ad7d21ab87
982956727 <982956727@qq.com>
2023-11-10 19:11:07 +0800

    从 iconfont 引入图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="topnavAndBanner">
-    <Topnav />
-    <div class="banner">
-      <h1>轱辘UI</h1>
-      <h2>一个厉害的 UI 框架</h2>
-      <p class="actions">
-        <a href="https://github.com">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav />
+      <div class="banner">
+        <h1>轱辘UI</h1>
+        <h2>一个厉害的 UI 框架</h2>
+        <p class="actions">
+          <a href="https://github.com">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon">
+        <use xlink:href="#icon-vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-light"></use>
+      </svg>
     </div>
   </div>
 </template>
@@ -18,10 +31,20 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+$black: #000000;
+$border-radius: 4px;
+$color: #b1b1b1;
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(255,255,255,1) 0%, rgba(247,247,248,1) 100%);
 }
+.features {
+  > svg {
+    width: 64px;
+    height: 64px;
+  }
+}
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -31,13 +54,14 @@ export default {
     padding: 8px 0;
     a {
       margin: 0 8px;
-      background: #fff;
+      background: $black;
+      color: white;
       display: inline-block;;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover {
+        text-decoration: none;
+      }
     }
   }
 }
